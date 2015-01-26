647ede62baad87cb4a43b3108987bcc091fa53d5
Kristen <kawilliams3@gmail.com>
2015-01-26 14:48:16 -0600

    update

--- main.js
+++ main.js
@@ -7,24 +7,30 @@
 // Here's where discussion ended at:
 
 // ```js
-function encode(phrase){
+function encode(phrase) {
     var result = "";
     // .. for each character in phrase
-    if( var i = 0; i < phrase.length; i++){
-        // get the charCode if letter at index i
+    for (var i = 0; i < phrase.length; i++) {
+        // get the charCode of letter at index i
         var cc = phrase[i].charCodeAt(0);
         // add 13 to the letter's charCode
-
-        result += String.fromCharCode(cc+13);
+        cc += 13;
+        // check if cc > 122,
+        // ... if it is, then get the difference b/w cc and 123 (cc-123) and add it to 97 ('a')
+            if ((cc += 13) > 122) {
+                result === ((cc += 13) -123) + 97;
+                // ((cc += 13) - 123) += 97;
+            }
+        result += String.fromCharCode(cc += 13);
     }
 
-    else(123 = cc+13 <= 135 ) {
-    result += String.fromCharCode((cc+13)-26);
+    // else(123 = cc + 13 <= 135) {
+    //     result += String.fromCharCode((cc + 13) - 26);
 
-}
+    // }
 
     // {
-        // [123 = "a", 124 = "b", 125 = "c", 126 = "d", 127 = "e", 128 = "f", 129 = "g", 130 = "h", 131 = "i", 132 = "j", 133 = "k", 134 = "l", 135 = "m"];
+    // [123 = "a", 124 = "b", 125 = "c", 126 = "d", 127 = "e", 128 = "f", 129 = "g", 130 = "h", 131 = "i", 132 = "j", 133 = "k", 134 = "l", 135 = "m"];
     // }
 
     return result;
@@ -32,21 +38,22 @@ function encode(phrase){
 
 console.log(encode("hello")); //---> "uryyb"
 
-function decode(phrase){
+ function decode(phrase) {
     var result = "";
     // .. for each character in phrase
-    if( var i = 0; i < phrase.length; i++){
+    for (var i = 0; i < phrase.length; i++) {
         // get the charCode if letter at index i
         var cc = phrase[i].charCodeAt(0);
         // add 13 to the letter's charCode
+        cc -= 13;
+            if ((cc -= 13) < 97) {
+               result === (cc -= 13) + 26;
+            }
+    result += String.fromCharCode(cc -= 13);
+    // } else(84 = cc - 13 <= 96) {
+        // result += String.fromCharCode((cc - 13) + 26);
 
-        result += String.fromCharCode(cc-13);
     }
-
-    else(84 = cc-13 <= 96 ) {
-    result += String.fromCharCode((cc-13)+26);
-
-}
     return result;
 }
 
@@ -58,15 +65,19 @@ console.log(decode("uryyb")); //---> "hello"
 // In addition to the [Easy Mode](#easy-mode), make `encode2()` and `decode2()` take an optional, extra argument that takes a `rotation`:
 
 // ```js
-function encode2(phrase, rotation){
+function encode2(phrase, rotation) {
 
 }
 
 console.log(encode("hello")); //---> "uryyb"
 
-function decode2(phrase, rotation){
+function decode2(phrase, rotation) {
 
 }
 
 console.log(decode("uryyb")); //---> "hello"
 // ```
+
+
+// http://unicode-table.com/en/#control-character
+
